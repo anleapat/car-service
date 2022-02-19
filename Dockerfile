34351b1b670c8fb13543ba9401006afca17fb21f
anleapat <dallasspan@hotmail.com>
2022-02-19 21:48:05 +0800

    kubernetes & jenkins build

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM openjdk:8-slim
-LABEL application=car-service-1.0
+LABEL application=car-service
 
 RUN bash -c 'mkdir -p /app/lib'
 
-COPY target/*.jar /app/lib/car-service-1.0-SNAPSHOT.jar
+COPY target/*.jar /app/lib/
 
 ENTRYPOINT ["java", "-jar", "/app/lib/car-service-1.0-SNAPSHOT.jar"]
--- build.sh
+++ build.sh
@@ -2,15 +2,32 @@
 set -x
 pwd
 ls
-mvn clean package -Dmaven.test.skip=true -U --settings settings.xml
 
-VERSION=`date "+%Y%m%d-%H%M%S"`
-PACKAGE=`ls target/*jar | awk -F '/' '{print $2}' | awk -F '-SNAPSHOT.jar' '{print $1}'`
-docker build -t ${PACKAGE}:${VERSION} .
+WORKSPACE=parking
+TAG=`date "+%Y%m%d%H%M%S-%N"`
+APPLICATION=car-service
+REGISTRY_URL=192.168.56.231:32098
 
-IMAGE_ID=`docker images | grep ${PACKAGE} | grep ${VERSION} | awk -F ' ' '{print $3}'`
-docker tag ${IMAGE_ID} parking-registry-svc.parking:5000/${PACKAGE}:${VERSION}
-docker push parking-registry-svc.parking:5000/${PACKAGE}:${VERSION}
+docker build -t ${APPLICATION}:${TAG} .
 
-kubectl create deploy ${PACKAGE} -n parking --image=parking-registry-svc.parking:5000/${PACKAGE}:${VERSION}
-docker rmi -f `docker images -f "label=application=${PACKAGE}" -q`
+IMAGE_ID=`docker images | grep ${APPLICATION} | grep ${TAG} | awk -F ' ' '{print $3}'`
+docker tag ${IMAGE_ID} $REGISTRY_URL/${APPLICATION}:${TAG}
+docker push $REGISTRY_URL/${APPLICATION}:${TAG}
+
+docker rmi -f `docker images -f "label=application=${APPLICATION}" -q | sort -u`
+
+LINE=`kubectl get deployment -n $WORKSPACE | grep ${APPLICATION} | wc -l`
+if [ $LINE -gt 0 ]; then
+	for POD in `kubectl get pod -n $WORKSPACE | grep $APPLICATION | awk -F ' ' '{print $1}'`
+	do
+		CONTAINER_NAME=`kubectl describe pod -n $WORKSPACE $POD | grep 'container' | head -1 | awk -F ' ' '{print $NF}'`
+		kubectl set image deployment/${APPLICATION} -n $WORKSPACE $CONTAINER_NAME=$REGISTRY_URL/${APPLICATION}:${TAG} --record
+	done
+else
+	if [ "X$1" != "X"  ]; then
+		DEPLOY_PARAM="--replicas=$1"
+	fi
+	
+	kubectl create deployment ${APPLICATION} -n $WORKSPACE --image=$REGISTRY_URL/${APPLICATION}:${TAG} $DEPLOY_PARAM
+fi
+
