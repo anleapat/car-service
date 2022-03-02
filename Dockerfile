23246cbbc55d8b65f085bbb352598b15f34a9875
anleapat <dallasspan@hotmail.com>
2022-03-02 19:39:29 +0800

    jenkins & kubernetes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM 192.168.1.37:5000/openjdk:8-slim
+FROM 192.168.1.37:5000/openjdk:8-buster
+
 LABEL application=car-service
 
 RUN bash -c 'mkdir -p /app/lib'
 
-COPY target/*.jar /app/lib/
+COPY target/*.jar /app/lib
 
 ENTRYPOINT ["java", "-jar", "/app/lib/car-service-1.0-SNAPSHOT.jar"]
--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 set -x
 
-WORKSPACE=$1
+NAMESPACE=$1
 IMAGE=$2
 APPLICATION=$3
 REPLICAS=$4
 
-LINE=`kubectl get deployment -n $WORKSPACE | grep ${APPLICATION} | wc -l`
+LINE=`kubectl get deployment -n $NAMESPACE | grep ${APPLICATION} | wc -l`
 if [ $LINE -gt 0 ]; then
-	for POD in `kubectl get pod -n $WORKSPACE | grep $APPLICATION | awk -F ' ' '{print $1}'`
-	do
-		CONTAINER_NAME=`kubectl describe pod -n $WORKSPACE $POD | grep 'container' | head -1 | awk -F ' ' '{print $NF}'`
-		kubectl set image deployment/${APPLICATION} -n $WORKSPACE $CONTAINER_NAME=$IMAGE $record
-	done
+	kubectl set image deployment/${APPLICATION} -n $NAMESPACE *=$IMAGE --record
+	POD_NUMBER=`kubectl get deployment/$APPLICATION -n $NAMESPACE | grep $APPLICATION | awk -F ' ' '{print $4}'`
+	if [ $POD_NUMBER -ne $REPLICAS ]; then
+		kubectl scale -n $NAMESPACE deployment $APPLICATION --replicas=$REPLICAS
+	fi
 else
 	if [ "X$1" != "X"  ]; then
 		DEPLOY_PARAM="--replicas=$REPLICAS"
 	fi
 	
-	kubectl create deployment ${APPLICATION} -n $WORKSPACE --image=${IMAGE} $DEPLOY_PARAM
+	kubectl create deployment ${APPLICATION} -n $NAMESPACE --image=${IMAGE} $DEPLOY_PARAM
 fi
 
