ed311b39e693db34b190a1cc25329477862a6d7d
anleapat <dallasspan@hotmail.com>
2022-05-02 10:10:42 +0800

    deploy with yaml

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,14 @@ IMAGE=$2
 APPLICATION=$3
 REPLICAS=$4
 
-LINE=`kubectl get deployment -n $NAMESPACE | grep ${APPLICATION} | wc -l`
-if [ $LINE -gt 0 ]; then
-	kubectl set image deployment/${APPLICATION} -n $NAMESPACE *=$IMAGE --record
-	POD_NUMBER=`kubectl get deployment/$APPLICATION -n $NAMESPACE | grep $APPLICATION | awk -F ' ' '{print $4}'`
-	if [ $POD_NUMBER -ne $REPLICAS ]; then
-		kubectl scale -n $NAMESPACE deployment $APPLICATION --replicas=$REPLICAS
-	fi
-else
-	if [ "X$1" != "X"  ]; then
-		DEPLOY_PARAM="--replicas=$REPLICAS"
-	fi
-	
-	kubectl create deployment ${APPLICATION} -n $NAMESPACE --image=${IMAGE} $DEPLOY_PARAM
+#replace params
+sed -i '''s/replicas_param/'$REPLICAS'/''' car-service.yaml
+sed -i '''s/tag_param/'$IMAGE'/''' car-service.yaml
+
+IS_EXISTS=`kubectl get deployment/$APPLICATION -n $NAMESPACE | grep $APPLICATION`
+if [ "X$IS_EXISTS" != "X" ]; then
+  kubectl delete -f car-service.yaml
 fi
 
+#deploy
+kubectl create -f car-service.yaml
